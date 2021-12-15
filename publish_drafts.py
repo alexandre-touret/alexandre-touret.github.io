2b462040c496baa720aa49462a3b859ccf90de8a
Alexandre Touret <alexandre.touret@free.fr>
2021-12-15 14:49:06 +0100

    Fin script python

--- publish_drafts.py
+++ publish_drafts.py
@@ -10,15 +10,22 @@ def publishFile(file):
             matched=re.match('date:(.*)',line)
             if(bool(matched)):
                 datetime_object = datetime.fromisoformat(line[6:])
-                basename=os.path.basename(file)
-                dest=POSTS_FOLDER+datetime_object.strftime("%Y-%m-%-d")+"-"+basename
-                print("Copying draft to [",dest,"]")
-                shutil.copy(file,dest)   
-                print("Draft published")
+                # Only publish if the date specified in the file is older than now()
+                if(datetime_object < datetime.now()):
+                    basename=os.path.basename(file)
+                    dest=POSTS_FOLDER+"/"+datetime_object.strftime("%Y-%m-%-d")+"-"+basename
+                    print("Copying draft [",file,"] to [",dest,"]")
+                    shutil.copy(file,dest)   
+                    print("Draft published")
+                else:
+                    print("This file [",file,"] is newer than now")
 
 def main():
     for file in glob.glob(DRAFTS_FOLDER+"/*.md", recursive = False):
+        print("------------------------------------------------")
+        print("Handling [",file,"]")
         publishFile(file)
+        print("------------------------------------------------")
 
 if __name__ == "__main__":
     DRAFTS_FOLDER = os.getenv('DRAFTS_FOLDER', '_drafts')
